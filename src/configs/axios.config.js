f02aba50578da031b1716703c4914198bdec0552
Igor Escalona <igorer88@gmail.com>
2022-10-19 23:05:00 -0400

    Add axios configs

---
+++ src/configs/axios.config.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const apiHost = import.meta.env.VITE_API_HOST;
+const apiVersion = import.meta.env.VITE_API_VERSION;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const baseURL = `${apiHost}/${apiVersion}/`;
+
+const axiosInstance = axios.create({
+  baseURL
+});
+
+axiosInstance.interceptors.request.use(
+  config => {
+    if (apiKey) {
+      config.headers.common['X-Auth-Token'] = apiKey;
+    }
+
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  response => {
+    return response.data;
+  },
+  error => {
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
